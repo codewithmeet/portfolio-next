7f0c6636f17034f95343c4ea3d98039cfc66b9b1
Meet Mandaviya <codewithmeet@gmail.com>
2023-05-25 12:04:48 +0530

    Add console.log

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -19,17 +19,20 @@ export default function (req, res) {
   transporter.verify(function (error, success) {
     if (error) {
       console.log("Error in connectingSMTP", error);
+      return;
     }
+    console.log("EMAIL ACCESSED...");
   });
-
   const mailData = {
     from: process.env.MAIL,
     to: req.body.email,
     subject: `LEAD GENERATED!!!!! Message From ${req.body.firstName} ${req.body.lastName}`,
     text: `This is client the client queries please check\nClient's Email: ${req.body.email} \nQueries: ${req.body.queries}`,
   };
-
+  console.log("EMAIL DATA ACCESSED...");
   transporter.sendMail(mailData, function (err, info) {
+    console.log("EMAIL SENDING...");
+    console.log("EMAIL SENT...");
     if (err) console.log("Error in sending email", err);
   });
   res.status(200).send();
